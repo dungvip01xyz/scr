2e3f848f2e67b3b2557af0ce548f610f090e461c
dungvip01xyz <168471127+dungvip01xyz@users.noreply.github.com>
2025-04-03 12:46:26 +0700

    Update hopvip.lua

--- hopvip.lua
+++ hopvip.lua
@@ -1,59 +1,61 @@
-function HopLower()
-    local maxplayers, gamelink, goodserver, data_table = math.huge, "https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100"
-    if not _G.FailedServerID then _G.FailedServerID = {} end
-    local function serversearch()
-        data_table = game:GetService"HttpService":JSONDecode(game:HttpGetAsync(gamelink))
-        for _, v in pairs(data_table.data) do
-            pcall(function()
-                if type(v) == "table" and v.id and v.playing and tonumber(maxplayers) > tonumber(v.playing) and not table.find(_G.FailedServerID, v.id) then
-                    maxplayers = v.playing
-                    goodserver = v.id
-                end
-            end)
+function Hop()
+    local v372 = game.PlaceId;
+    local v373 = {};
+    local v374 = "";
+    local v375 = os.date("!*t").hour;
+    local v376 = false;
+    function TPReturner()
+        local v556;
+        if (v374 == "") then
+            v556 = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/" .. v372 .. "/servers/Public?sortOrder=Asc&limit=100"));
+        else
+            v556 = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/" .. v372 .. "/servers/Public?sortOrder=Asc&limit=100&cursor=" .. v374));
         end
-    end
-    function getservers()
-        pcall(serversearch)
-        for i, v in pairs(data_table) do
-            if i == "nextPageCursor" then
-                if gamelink:find"&cursor=" then
-                    local a = gamelink:find"&cursor="
-                    local b = gamelink:sub(a)
-                    gamelink = gamelink:gsub(b, "")
-                end
-                gamelink = gamelink .. "&cursor=" .. v
-                pcall(getservers)
-            end
+        local v557 = "";
+        if (v556.nextPageCursor and (v556.nextPageCursor ~= "null") and (v556.nextPageCursor ~= nil)) then
+            v374 = v556.nextPageCursor;
         end
-    end
-    pcall(getservers)
-    wait(.1)
-    if goodserver == game.JobId or maxplayers == #game:GetService"Players":GetChildren() - 1 then
-    end
-    table.insert(_G.FailedServerID, goodserver)
-    game:GetService"TeleportService":TeleportToPlaceInstance(game.PlaceId, goodserver)
-
-    while wait(.1) do
-        pcall(function()
-            if not game:IsLoaded() then
-                game.Loaded:Wait()
-            end
-            game.CoreGui.RobloxPromptGui.promptOverlay.DescendantAdded:Connect(function()
-                local GUI = game.CoreGui.RobloxPromptGui.promptOverlay:FindFirstChild("ErrorPrompt")
-                if GUI then
-                    if GUI.TitleFrame.ErrorTitle.Text == "Disconnected" then
-                        if #game.Players:GetPlayers() <= 1 then
-                            game.Players.LocalPlayer:Kick("\nRejoining...")
-                            wait(.1)
-                            game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
-                        else
-                            game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId, game.Players.LocalPlayer)
+        local v558 = 0;
+        for v651, v652 in pairs(v556.data) do
+            local v653 = true;
+            v557 = tostring(v652.id);
+            if (tonumber(v652.maxPlayers) > tonumber(v652.playing)) then
+                for v872, v873 in pairs(v373) do
+                    if (v558 ~= 0) then
+                        if (v557 == tostring(v873)) then
+                            v653 = false;
                         end
+                    elseif (tonumber(v375) ~= tonumber(v873)) then
+                        local v1472 = pcall(function()
+                            v373 = {};
+                            table.insert(v373, v375);
+                        end);
                     end
+                    v558 = v558 + 1 ;
                 end
-            end)
-        end)
+                if (v653 == true) then
+                    table.insert(v373, v557);
+                    wait();
+                    pcall(function()
+                        wait();
+                        game:GetService("TeleportService"):TeleportToPlaceInstance(v372, v557, game.Players.LocalPlayer);
+                    end);
+                    wait();
+                end
+            end
+        end
     end
+    function v118()
+        while wait() do
+            pcall(function()
+                TPReturner();
+                if (v374 ~= "") then
+                    TPReturner();
+                end
+            end);
+        end
+    end
+    v118();
 end
 function SuperFixLagMAX()
 	wait(10)
@@ -131,8 +133,9 @@ for _, playerName in ipairs(playerNames) do
         local player = Players:FindFirstChild(playerName)
         if player then
             print(playerName .. " đang ở trong server, tôi thoát game.")
-            HopLower()
-			HopLower()
+            task.spawn(Hop)
+            task.spawn(Hop)
+            task.spawn(Hop)
             break 
         else
             print(playerName .. " không có trong server, tôi ở lại.")
