27326c04c98c2852feb2fb23c227d55431ef0ff6
dungvip01xyz <168471127+dungvip01xyz@users.noreply.github.com>
2025-05-07 22:48:36 +0700

    Create hopvipv0.lua

---
+++ hopvipv0.lua
@@ -0,0 +1,80 @@
+local Players = game:GetService("Players")
+local localPlayer = Players.LocalPlayer 
+local HttpService = game:GetService("HttpService")
+local playerNames = getgenv().CheckpPlayer
+function Hop()
+    local v372 = game.PlaceId;
+    local v373 = {};
+    local v374 = "";
+    local v375 = os.date("!*t").hour;
+    local v376 = false;
+    function TPReturner()
+        local v556;
+        if (v374 == "") then
+            v556 = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/" .. v372 .. "/servers/Public?sortOrder=Asc&limit=100"));
+        else
+            v556 = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/" .. v372 .. "/servers/Public?sortOrder=Asc&limit=100&cursor=" .. v374));
+        end
+        local v557 = "";
+        if (v556.nextPageCursor and (v556.nextPageCursor ~= "null") and (v556.nextPageCursor ~= nil)) then
+            v374 = v556.nextPageCursor;
+        end
+        local v558 = 0;
+        for v651, v652 in pairs(v556.data) do
+            local v653 = true;
+            v557 = tostring(v652.id);
+            if (tonumber(v652.maxPlayers) > tonumber(v652.playing)) then
+                for v872, v873 in pairs(v373) do
+                    if (v558 ~= 0) then
+                        if (v557 == tostring(v873)) then
+                            v653 = false;
+                        end
+                    elseif (tonumber(v375) ~= tonumber(v873)) then
+                        local v1472 = pcall(function()
+                            v373 = {};
+                            table.insert(v373, v375);
+                        end);
+                    end
+                    v558 = v558 + 1 ;
+                end
+                if (v653 == true) then
+                    table.insert(v373, v557);
+                    wait();
+                    pcall(function()
+                        wait();
+                        game:GetService("TeleportService"):TeleportToPlaceInstance(v372, v557, game.Players.LocalPlayer);
+                    end);
+                    wait();
+                end
+            end
+        end
+    end
+    function v118()
+        while wait() do
+            pcall(function()
+                TPReturner();
+                if (v374 ~= "") then
+                    TPReturner();
+                end
+            end);
+        end
+    end
+    v118();
+end
+for _, playerName in ipairs(playerNames) do 
+    if playerName == localPlayer.Name then
+        print("Tên tôi có trong danh sách, bỏ qua kiểm tra.") 
+    else
+        local player = Players:FindFirstChild(playerName)
+        if player then
+            print(playerName .. " đang ở trong server, tôi thoát game.")
+            while true do
+                task.spawn(Hop)
+                task.wait(1) -- nên có delay nếu không thì Roblox sẽ crash hoặc lag
+            end            
+            break 
+        else
+            print(playerName .. " không có trong server, tôi ở lại.")
+        end
+    end
+end
