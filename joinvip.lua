89e2c8e51e603fa61bccc9c0df887d01ab811230
dungvip01xyz <168471127+dungvip01xyz@users.noreply.github.com>
2025-12-05 23:46:05 +0700

    Add userId to playerData in joinvip.lua

--- joinvip.lua
+++ joinvip.lua
@@ -146,6 +146,7 @@ local function Getdata()
             local playerData = {
                 DisplayName = localPlayer.DisplayName,
                 Username = localPlayer.Name,
+                userId = localPlayer.UserId
                 Level = localPlayer.Data.Level.Value,
                 Beli = localPlayer.Data.Beli.Value,
                 Fragments = localPlayer.Data.Fragments.Value,
